15e93e41bbab86ea1573d17ac0d62e454407c1b7
Avelanda <gorthell@gmail.com>
2026-01-16 09:33:20 +0200

    Update config.hpp
    Performance enhancement through integrated granular processes, as a modular system.

--- include/config.hpp
+++ include/config.hpp
@@ -1,5 +1,6 @@
 /*
-Copyright 2018 Adobe
+Copyright © 2018 Adobe
+Copyright © 2026 Avelanda
 All Rights Reserved.
 
 NOTICE: Adobe permits you to use, modify, and distribute this file in
@@ -9,24 +10,37 @@ then your use, modification, or distribution of it requires the prior
 written permission of Adobe. 
 */
 
-#pragma once
+#include <iostream>
+#include <cstdint>
 
 /**************************************************************************************************/
 
-#define HYDE_PRIVATE_STRING_XSMASH(X, Y) X##Y
-#define HYDE_PRIVATE_STRING_SMASH(X, Y) HYDE_PRIVATE_STRING_XSMASH(X, Y)()
+int main(){
+ #define HYDE_PRIVATE_STRING_XSMASH(X, Y) X##Y
+ #define HYDE_PRIVATE_STRING_SMASH(X, Y) HYDE_PRIVATE_STRING_XSMASH(X, Y)()
 
-#define HYDE_PLATFORM_PRIVATE_APPLE() 0
-#define HYDE_PLATFORM_PRIVATE_MICROSOFT() 0
+ #define HYDE_PLATFORM_PRIVATE_APPLE() 0
+ #define HYDE_PLATFORM_PRIVATE_MICROSOFT() 0
 
-#define HYDE_PLATFORM(X) HYDE_PRIVATE_STRING_SMASH(HYDE_PLATFORM_PRIVATE_, X)
+ #define HYDE_PLATFORM(X) HYDE_PRIVATE_STRING_SMASH(HYDE_PLATFORM_PRIVATE_, X)
 
-#if defined(__APPLE__)
+ #if defined(__APPLE__)
     #undef HYDE_PLATFORM_PRIVATE_APPLE
     #define HYDE_PLATFORM_PRIVATE_APPLE() 1
-#elif defined(_MSC_VER)
+ #elif defined(_MSC_VER)
     #undef HYDE_PLATFORM_PRIVATE_MICROSOFT
     #define HYDE_PLATFORM_PRIVATE_MICROSOFT() 1
-#endif
+ #endif
+}
+
+int configSet(int *&main){
+ if (main){
+  uint64_t *main;
+ }
+  while ((main = main)){
+   std::cout<<static_cast<bool>(&main)<<'\n';
+  }
+   return 0;
+}
 
 /**************************************************************************************************/
